f0bf88be4012f31b5c330acba3bd83d3cd7c6b15
yz13019-cpu <yz13019@nyu.edu>
2026-09-09 23:40:26 +0800

    Update sketch.js

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,9 +2,9 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
+  { text: "Creative coding is 'the intersection of programming, design, and art'", source: "Scott" },
+  { text: "Creative coding is ‘a chance for code to be wrong’ ", source: "Sara" },
+  { text: "Creative coding is ‘surprise, exploration, and aesthetic exploration’", source: "Sally" },
   { text: "process over results", source: "Sam" },
   { text: "form over function", source: "Sean" },
   { text: "iterative and experimental", source: "Scooby" },
